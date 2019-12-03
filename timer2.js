0a89d7af9eee5c0e7f0c8ed7b072905866d83176
melonfruity <eric.luo24@gmail.com>
2019-12-03 13:20:39 -0500

    initial commit of timer2.js

---
+++ timer2.js
@@ -0,0 +1,24 @@
+
+
+const stdin = process.stdin;
+// don't worry about these next two lines of setup work.
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+////////////
+// Event Handling for User Input
+////////////
+
+// on any input from stdin (standard input), output a "." to stdout
+stdin.on('data', (value) => {
+    // \u0003 maps to ctrl+c input
+    if (value === '\u0003') {
+    process.exit();
+  }
+  if (Number(value) && Number(value) > 0 && typeof Number(value) === 'number') {
+    setTimeout(() => {
+        process.stdout.write(value);
+        process.stdout.write('\x07');
+    }, value * 1000);
+    }
+});
